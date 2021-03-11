779544b0a9104171d0bea96ab65278b05d25a8e7
sanjay037 <sanjaykumar.reddy@iiitb.org>
2021-03-11 11:17:24 +0530

    Fixed issue in scanner

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -12,7 +12,6 @@ public class Calculator
         {
             System.out.print("Select the operation to be performed from below options\n");
             System.out.println("1.Square Root\n 2.Factorial\n 3.Exit");
-            n = 3;
             if(input.hasNextInt()) n = input.nextInt();
             switch (n) {
                 case 1:
